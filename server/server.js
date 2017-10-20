c29258fb583fa8e34670a39c28e3b3491b3f9e12
Erick.S <esatire@gmail.com>
2017-10-20 12:40:43 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const publicPath = path.join(__dirname, '../public');
+
+const PORT = 3000;
+
+var app = express()
+
+// Static files
+app.use(express.static(publicPath));
+
+var server = app.listen(PORT, () => {
+    console.log("Server up at port " + PORT)
+})
