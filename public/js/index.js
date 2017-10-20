10029a20dcf0e0acb7048920424e3877f0da2394
Erick.S <esatire@gmail.com>
2017-10-20 15:58:28 +0300

    Greet new user and alert others.

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,6 @@
 var socket = io()
 socket.on('connect', function() {
     console.log("Connected, dude")
-
-    socket.emit('createEmail', {
-        from: "arikshtain@gmail.com",
-        to: "somehotty@hotmail.com",
-        text: "Hi this is me, your lord and master",
-        createdAt: new Date().getTime()
-    })
-
-    socket.emit('createMessage', {
-        from: "me",
-        to: "someone",
-        text: "Test text"
-    })
-
-    socket.emit('createMessage', {
-        from: "someone",
-        to: "me",
-        text: "B00bs!"
-    })
 })
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -18,10 +18,13 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New user connected.")
 
+    socket.emit("newMessage", { from: "Admin", text: "Welcome to the chat."})
+    socket.broadcast.emit("newMessage", {from: "Admin", text: "A new user has joined the chat."})
+
     socket.on('createMessage', (message) => {
         message.createdAt = new Date().getTime()
         console.log("New message created", message)
-        socket.emit("newMessage", message)
+        io.emit("newMessage", message)
     })
 
     socket.on('disconnect', () => {
